681edd5dce5432e911c9ac32e294b72a10b53376
HyperClap <MrGromFullYou@yandex.ru>
2024-12-18 20:11:23 +0300

    fixed empty about window.

--- NoteApp/obj/Debug/net8.0-windows/NoteApp.AssemblyInfo.cs
+++ NoteApp/obj/Debug/net8.0-windows/NoteApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NoteApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f5dfb5a0d5bb4c3c6225795d3098738925a5578")]
 [assembly: System.Reflection.AssemblyProductAttribute("NoteApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NoteApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
